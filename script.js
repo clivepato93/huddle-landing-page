ce030428b5e8d4d3363fb107b5a96195b1c6bc70
clivepato93 <clivetoniato1@hotmail.com>
2022-04-22 23:51:45 +0100

    test

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-document.addEventListener("click", function(e) {}, true);
+// document.addEventListener("click", function(e) {}, true);
+
+const btn= document.querySelector('.btn');
+console.log(btn)
+btn.addEventListener('click',function(){
+    btn.classList.toggle('clicked')
+})
 
 console.log('💩')
 
