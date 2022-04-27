6abdfad7dcdd5a472480abd34254138f2c8c3255
clivepato93 <clivetoniato1@hotmail.com>
2022-04-27 11:36:11 +0100

    test

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
 // document.addEventListener("click", function(e) {}, true);
 
-const btn= document.querySelector('.btn');
-const socialBtns = document.querySelectorAll('.btnStyle');
-console.log(socialBtns)
+// const btn= document.querySelector('.btn');
+// const socialBtns = document.querySelectorAll('.btnStyle');
+// const socialIcons = document.querySelectorAll('.socialBtn');
+// console.log(socialBtns)
 
-socialBtns.forEach(btn=>{
-    btn.addEventListener('click',function (e) {
-        const child = btn.firstElementChild;
-        console.log(child)
+// socialBtns.forEach(btn=>{
+//     btn.addEventListener('click',function (e) {
+//         const target = e.target;
+//         socialBtns.forEach(button=>{
+//             const child = button.firstElementChild;
+//             if(button!==target){
+//                 button.classList.remove('click');
+//                 child.classList.remove('socialBtnClick')
+//             }
+//             else{
+//                 target.classList.toggle('click');
+//                 child.classList.toggle('socialBtnClick')
+//             }
+
+//         })
+
+        
+        // console.log(child)
         // console.log(e.target,e.target)
 
-        btn.classList.toggle('click');
-        child.classList.toggle('socialBtnClick')
         // btn.classList.toggle('click');
+        // child.classList.toggle('socialBtnClick')
+        // btn.classList.toggle('click');
+//     })
+// })
+
+/*
+socialIcons.forEach(icon=>{
+    icon.addEventListener('click',function (e) {
+        const target = e.target;
+        socialIcons.forEach(icon=>{
+            const parent = icon.parentElement;
+            if(icon!==target){
+                icon.classList.remove('socialBtnClick')
+                parent.classList.remove('click');
+            }
+            else{
+                icon.classList.add('socialBtnClick');
+                parent.classList.toggle('click')
+            }
+
+        })
+
     })
-})
+})*/
 
-btn.addEventListener('click',function(){
-    btn.classList.toggle('clicked')
-})
+// btn.addEventListener('click',function(){
+//     btn.classList.toggle('clicked')
+// })
 
-console.log('💩')
+// console.log('💩')
 
 window.addEventListener('resize',function () {
     console.log(innerWidth)
